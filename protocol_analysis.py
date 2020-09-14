aa7df002c304e44a13929804569b53f24111779b
Ice Man <tshemm@gmail.com>
2020-09-14 14:35:17 +0200

    final commit with fully working script

--- protocol_analysis.py
+++ protocol_analysis.py
@@ -7,6 +7,7 @@ import geoip2.database
 import time
 import subprocess
 from pprint import pprint
+import sys, getopt
 from datetime import datetime
 from ripe.atlas.cousteau import (
   Ping,
@@ -72,7 +73,7 @@ def send_ripe_measurement_request(website, ATLAS_API_KEY):
 	result_id = []
 	if is_success:
 		result_id = response['measurements']
-		print(result_id)
+		print("Measurement for",website,"sent. Result ID is",result_id[0])
 		return result_id
 	else:
 		print("Measurement not successful")
@@ -129,9 +130,8 @@ def check_ipv6(website, myResolver):
 
 def find_cdns(trace_data):
 	filename = trace_data
-	asn_path = []
-	pathToDb = "C:/Users/tshit/Documents/Blessed/Honours Courses/Honors Project/geolite_dbs/GeoLite2-City_20200721/GeoLite2-City.mmdb"
-	pathToAsnDb = "C:/Users/tshit/Documents/Blessed/Honours Courses/Honors Project/geolite_dbs/GeoLite2-ASN_20200721/GeoLite2-ASN.mmdb"
+	pathToDb = "geolite_dbs/GeoLite2-City_20200721/GeoLite2-City.mmdb"
+	pathToAsnDb = "geolite_dbs/GeoLite2-ASN_20200721/GeoLite2-ASN.mmdb"
 
 	with geoip2.database.Reader(pathToDb) as reader:
 		with geoip2.database.Reader(pathToAsnDb) as asn_reader:
@@ -143,11 +143,9 @@ def find_cdns(trace_data):
 					try:
 						asn_response = asn_reader.asn(result['src_addr'])
 						asn = asn_response.autonomous_system_number
-						asn_path.append(str(asn))
 					except:
 						asn = "Source ASN unknown"
-						asn_path.append(asn)
-					print("Result number",count,"from address",result['src_addr'],"from ASN",asn)
+					print("Result number",count,"from address:",result['src_addr'],"from ASN:",asn)
 					count+=1
 					for tracert in result['result']:
 						#first try get tracert['result'][1]['from'] if it exists
@@ -158,58 +156,109 @@ def find_cdns(trace_data):
 								asn_response = asn_reader.asn(hop_ip)
 								response = reader.city(hop_ip)
 								asn = asn_response.autonomous_system_number
-								asn_path.append("->"+str(asn))
 								city = response.city.name
 								print(hop_ip, city, asn)
 							except:
 								print("IP not in database")
-					print("ASN path taken from address",result['src_addr'],"to destination:")
-					print_asn_paths(list(dict.fromkeys(asn_path)))
-					asn_path.clear()
 					print("")
 
-def print_asn_paths(asn_path):
-	for asn in asn_path:
-		print(asn, end="")
-	print('\n')
+def print_asn_paths(trace_data, website):
+	filename = trace_data
+	pathToDb = "geolite_dbs/GeoLite2-City_20200721/GeoLite2-City.mmdb"
+	pathToAsnDb = "geolite_dbs/GeoLite2-ASN_20200721/GeoLite2-ASN.mmdb"
 
-def main(argv):
+	with geoip2.database.Reader(pathToDb) as reader:
+		with geoip2.database.Reader(pathToAsnDb) as asn_reader:
+			with open(filename) as json_file:
+				data = json.load(json_file)
+				count=1
+				#loop through each array item in the json and geolocate the destination address in each trace hop
+				for result in data:
+					print("ASN path taken from address",result['src_addr'],"to",website,":")
+					try:
+						asn_response = asn_reader.asn(result['src_addr'])
+						asn = asn_response.autonomous_system_number
+						print(asn, end="")
+					except:
+						print("Source ASN unknown", end="")
+					count+=1
+					for tracert in result['result']:
+						#first try get tracert['result'][1]['from'] if it exists
+						if tracert['result'][1]!={"x": "*"}:
+							hop_ip = tracert['result'][1]['from']
+							#geolocate the ip
+							try:
+								asn_response = asn_reader.asn(hop_ip)
+								asn = asn_response.autonomous_system_number
+								print("->"+str(asn), end="")
+							except:
+								continue
+								#print("IP not in database")
+					print('\n')
+				print("")
+
+def main():
+
 	website_1 = ''
 	website_2 = ''
 	ATLAS_API_KEY = "9b81eb94-44fe-4e2e-aca7-508b94b7041b"
-	if len(sys.argv)==3:
-		website_1 = sys.argv[1]
-		website_2 = sys.argv[2]
-		myResolver = dns.resolver.Resolver()
+	myResolver = dns.resolver.Resolver()
 
-		#start with the nameservers of each website
-		# find_nameservers(website_1, myResolver)
-		# find_nameservers(website_2, myResolver)
-		# check_ipv4(website_1, myResolver)
-		# check_ipv4(website_2, myResolver)
-		# check_ipv6(website_1, myResolver)
-		# check_ipv6(website_2, myResolver)
-		# res_id_1 = send_ripe_measurement_request(website_1, ATLAS_API_KEY)
-		# res_id_2 = send_ripe_measurement_request(website_2, ATLAS_API_KEY)
-		res_id_1 = [27151070]
-		res_id_2 = [27151071]
-		# print("Waiting 15mins for Ripe results...")
-		# time.sleep(900)
-		# if len(res_id_1)!=0:
-		# 	fetch_ripe_result(res_id_1[0], "website_1_trace.txt")
-		# else:
-		# 	print("Results for", website_1,"could not be fetched")
+	#if the user puts less than 3 arguments
+	if len(sys.argv[1:])<=4:
+		print("usage: protocol_analysis --website1 <website_1> --website2 <website_2> --OPTION")
+		print("where OPTION in [--measure, --help, --asnlinks, --dns, --traces]")
+		sys.exit()
 
-		# if len(res_id_2)!=0:
-		# 	fetch_ripe_result(res_id_2[0], "website_2_trace.txt")
-		# else:
-		# 	print("Results for", website_2,"could not be fetched")
+	#options are --measure, -h/--help, --asnlinks, --dns
+	try:
+		opts, args = getopt.getopt(sys.argv[1:],"h",["website1=","website2=","measure", "traces","help", "asnlinks", "dns"])
+	except getopt.GetoptError:
+		print("usage: protocol_analysis --website1 <website_1> --website2 <website_2> --OPTION")
+		print("where OPTION in [--measure, --help, --asnlinks, --dns, --traces]")
+		sys.exit(2)
 
+	for opt, arg in opts:
+		if opt in ('-h', "--help"):
+			print("usage: protocol_analysis --website1 <website_1> --website2 <website_2> --OPTION")
+			print("where OPTION in [--measure, --help, --asnlinks, --dns, --traces]")
+			sys.exit()
+		elif opt == "--website1":
+			website_1 = arg
+		elif opt == "--website2":
+			website_2 = arg
+		elif opt == "--measure":
+			print("Beginning Ripe Atlas measurements...")
+			res_id_1 = send_ripe_measurement_request(website_1, ATLAS_API_KEY)
+			res_id_2 = send_ripe_measurement_request(website_2, ATLAS_API_KEY)
+			print("Waiting 15mins for Ripe results...")
+			time.sleep(900)
+			if len(res_id_1)!=0:
+				print("Writing the results of",website_1,"to website_1_trace.txt and will overwrite the existing file")
+				fetch_ripe_result(res_id_1[0], "website_1_trace.txt")
+			else:
+				print("Results for", website_1,"could not be fetched")
 
-		find_cdns("website_1_trace.txt")
-		find_cdns("website_2_trace.txt")
-		
-
+			if len(res_id_2)!=0:
+				print("Writing the results of",website_2,"to website_2_trace.txt and will overwrite the existing file")
+				fetch_ripe_result(res_id_2[0], "website_2_trace.txt")
+			else:
+				print("Results for", website_2,"could not be fetched")
+		elif opt == "--dns":
+			find_nameservers(website_1, myResolver)
+			find_nameservers(website_2, myResolver)
+			check_ipv4(website_1, myResolver)
+			check_ipv4(website_2, myResolver)
+			check_ipv6(website_1, myResolver)
+			check_ipv6(website_2, myResolver)
+		elif opt == "--traces":
+			print("Displaying traceroute results from Ripe with ASN and City added to each IP...")
+			time.sleep(5)
+			find_cdns("website_1_trace.txt")
+			find_cdns("website_2_trace.txt")
+		elif opt == "--asnlinks":
+			print_asn_paths("website_1_trace.txt", website_1)
+			print_asn_paths("website_2_trace.txt", website_2)
 
 
 		# #---------USE TRACEROUTE AND GEOLITE GEOLOCATION TOO FIND THE SERVERS--------
@@ -222,18 +271,5 @@ def main(argv):
 		# print(output)
 
 
-   		#------------USE RIPE ATLAS FOR TRACEROUTE MEASUREMENTS-----------------
-		
-
-		
-
-		
-				
-			#time.sleep(30)
-
-	else:
-		print("usage: protocol_analysis <website_1> <website_2>")
-
-
 if __name__ == "__main__":
-	main(sys.argv)
+	main()
